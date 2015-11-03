b78f799f130e1405df3afb1d42f370fe3c8f0337
Ryujin35 <ehaworth17@lausanneschool.com>
2015-11-03 16:46:00 -0600

    Update DoubleJoystickBoogaloo.java

--- DoubleJoystickBoogaloo.java
+++ DoubleJoystickBoogaloo.java
@@ -50,14 +50,8 @@ public class DoubleJoystickBoogaloo extends OpMode {
         motorRB = hardwareMap.dcMotor.get("motor_2");
         motorLF = hardwareMap.dcMotor.get("motor_3");
         motorLB = hardwareMap.dcMotor.get("motor_4");
-
     }
-    /*two joystick command
-    motorLF.setPower(ScalePower(Range.clip(gamepad1.left_stick_y)));
-    motorLB.setPower(ScalePower(Range.clip(gamepad1.left_stick_y)));
-    motorRB.setPower(ScalePower(Range.clip(gamepad1.right_stick_y)));
-    motorRF.setPower(ScalePower(Range.clip(gamepad1.right_stick_y)));
-    * */
+
     @Override
     public void loop(){ runFromDJB(); }
 
